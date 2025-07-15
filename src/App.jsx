b95960853dbeb0fb7c55d40eabf451f6414301b0
Poseryaev <kirill.vova@mail.ru>
2025-07-15 12:38:47 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+text-align: center;
+`;
+
+export const App = () => {
+
+	return (
+		<Div>
+		<i className="fa fa-envelope" aria-hidden="true"></i>
+		<div>123</div>
+		</Div>
+	);
+}
+
