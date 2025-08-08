0805978f34d56ecabe5d72f57459433f17dcd707
Poseryaev <kirill.vova@mail.ru>
2025-08-08 15:13:52 +0400

    Реализовать страницу пользователи

---
+++ src/pages/users/components/user-row/user-row.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { Icon } from '../../../../components';
+import { TableRow } from '../table-row/table-row';
+import { useServerRequest } from '../../../../hooks';
+import styled from 'styled-components';
+
+const UserRowContainer = ({
+	className,
+	id,
+	login,
+	registeredAt,
+	roleId: userRoleId,
+	roles,
+	onUserRemove,
+}) => {
+	const [initialRoleId, setInitialRoleId] = useState(userRoleId);
+	const [selectedRoleId, setSelectedRoleId] = useState(userRoleId);
+	const requestServer = useServerRequest();
+
+	const onRoleChange = ({ target }) => {
+		setSelectedRoleId(Number(target.value));
+	};
+
+	const onRoleSave = (userId, newUserRoleId) => {
+		requestServer('updateUserRole', userId, newUserRoleId).then(() => {
+			setInitialRoleId(newUserRoleId)
+		})
+	};
+	
+	const isSaveButtonDisaabled = selectedRoleId === initialRoleId;
+
+	return (
+		<div className={className}>
+			<TableRow border={true}>
+				<div className="login-column">{login}</div>
+				<div className="registered-at-column">{registeredAt}</div>
+				<div className="role-column">
+					<select value={selectedRoleId} onChange={onRoleChange}>
+						{roles.map(({ id: roleId, name: roleName }) => (
+							<option key={roleId} value={roleId}>
+								{roleName}
+							</option>
+						))}
+					</select>
+						<Icon
+							id="fa-floppy-o"
+							margin=" 0 0 0 10px"
+							disabled={isSaveButtonDisaabled}
+							onClick={() => onRoleSave(id, selectedRoleId)}
+						/>
+				</div>
+			</TableRow>
+			<Icon
+				id="fa-trash-o"
+				margin=" 0 0 0 10px"
+				onClick={onUserRemove}
+			/>
+		</div>
+	);
+};
+
+export const UserRow = styled(UserRowContainer)`
+	display: flex;
+	margin: 0 0 10px 0;
+	& select {
+		font-size: 16px;
+		margin: 5px 0 5px 0;
+		padding: 0 5px;
+	}
+`;
